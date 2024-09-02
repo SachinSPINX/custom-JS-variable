40c3b6338abddbad5534c25de7314c3de3d484d0
SachinSPINX <sachin.spinx@gmail.com>
2024-09-02 11:18:49 +0530

    Update custom-js-variable-for-referral-code-from-url.js

--- custom-js-variable-for-referral-code-from-url.js
+++ custom-js-variable-for-referral-code-from-url.js
@@ -6,4 +6,4 @@ function() {
 }
 
 
-//url should be like - https://www.abc.com/work/?referral-code=sachin
+//url should be like - https://www.abc.com/work/?referral-code=sachin. in the above code, you can replace the "referral-code" with whatever you will put in the URL.
